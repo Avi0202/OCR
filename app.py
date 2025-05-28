e850e3cfe664a84cea377d8726d522e4410f0a8c
Avi0202 <131533128+Avi0202@users.noreply.github.com>
2025-05-28 16:47:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pytesseract
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd =r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+
+uploaded_file=st.file_uploader("Upload an Image",type=["jpg","png","jpeg"])
+
+if uploaded_file:
+    image=Image.open(uploaded_file)
+    st.image(image,caption="Uploaded Image",width=300)
+    if st.button("Extract text"):
+        text=pytesseract.image_to_string(image)
+        st.text_area("Extracted text",text,height=200)
+        st.download_button("Download",text,file_name="extracted.txt")
+        pass
+    pass
