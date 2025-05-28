9e8d5e782ff2f16cfb218b1896463821937ef05d
Avi0202 <131533128+Avi0202@users.noreply.github.com>
2025-05-28 21:48:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import platform
 import streamlit as st
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd =r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+if platform.system()=="Windows":
+  pytesseract.pytesseract.tesseract_cmd =r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
 uploaded_file=st.file_uploader("Upload an Image",type=["jpg","png","jpeg"])
